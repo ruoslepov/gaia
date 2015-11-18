a490db5cb6b68e0490a6046cc124ebc02bef5ac2
Oleg Slepov <oleg.slepov@symphonyteleca.com>
2015-11-18 14:54:47 +0300

    Update notifications

--- apps/verticalhome/js/pinned_apps.js
+++ apps/verticalhome/js/pinned_apps.js
@@ -13,14 +13,18 @@
     var pinnedAppTitle = document.createElement('span');
     var pinnedList = document.getElementById('pinned-apps-list');
     var moreAppsLi = document.getElementById('moreApps');
+    var unreadNotif = document.createElement('div');
+
 
     pinnedElem.className = 'pinned-app-item';
     pinnedAppIcon.className = 'pinned-app-icon';
     pinnedAppTitle.className = 'title';
+    unreadNotif.className = 'unread_notif';
 
     pinnedElem.setAttribute('data-index', this.index);
     pinnedElem.appendChild(pinnedAppIcon);
     pinnedElem.appendChild(pinnedAppTitle);
+    pinnedElem.appendChild(unreadNotif);
 
     pinnedList.insertBefore(pinnedElem, moreAppsLi);
 
@@ -149,9 +153,11 @@
     PinnedAppsManager.instance = this;
 
     this.items = [];
+    this._storeRef = null;
   }
 
   PinnedAppsManager.prototype = {
+    STORE_NAME: 'notifications_count',
     init: function () {
       var pinnedAppsList = app.getPinnedAppsList();
       for (var i = 0; i < pinnedAppsList.length; i++) {
@@ -161,8 +167,174 @@
       this.items.sort(function(elem1, elem2) {
         return elem1.index - elem2.index;
       });
+
+      this.initStore().then(store => {
+        store.onchange = e => {
+          switch (e.operation) {
+          case 'removed':
+            this.debug('REMOVED last notification for ' + e.id);
+            ShowNotifBubble(e.id, 0);
+            return;
+          case 'cleared':
+            this.debug('ALL notifications were removed');
+            CleanAllBubbles();
+            return;
+          case 'added':
+            this.debug('added notification for new target');
+          case 'updated':
+            this.debug('UPDATED notification for existing target');
+          }
+
+          this._storeRef.get(e.id).then( count => {
+            this.debug('loaded value == ' + count + ' for ID = ' + e.id);
+            // trying to workaround bunch removal of notifications e.g. when opening 'Missed calls'
+            // screen, which leads to almost simultaneous 'close' of all missed calls notifications
+            if (count) {
+              ShowNotifBubble(e.id, count);
+            }
+          }, err => {
+            console.warn('[el][PinAppManager] error loading notifications count. err = ' + JSON.stringify(err));
+          });
+        };
+        LoadAllNotifications(store).then( data => {
+          Object.getOwnPropertyNames(data).forEach( item => {
+            this.debug('INIT: got ' + data[item] + ' for app = ' + item);
+            ShowNotifBubble(item, data[item]);
+          });
+        }, error => {
+          console.warn('[el] [PinAppManager] Problem loading previous notifications | err = ' + JSON.stringify(error));
+        });
+
+      }, err => {
+        console.warn('[el][PinAppManager] Problem opening \'notifications_count\' storage| err = ' + JSON.stringify(err));
+      });
+    },
+
+    debug: function pa_debug(msg) {
+      console.log('[el] ' + msg);
+    },
+
+    initStore: function pa_initStore() {
+      return new Promise(resolve => {
+        if (this._storeRef) {
+          return resolve(this._storeRef);
+        }
+        navigator.getDataStores(this.STORE_NAME).then(stores => {
+          this._storeRef = stores[0];
+          //TODO: need to iterate through obtained store, to read initial values for notifications.
+          return resolve(this._storeRef);
+        }, err => {
+          this.debug('ERROR '+ JSON.stringify(err));
+          return reject(err);
+        });
+      });
     }
   };
 
   exports.PinnedAppsManager = PinnedAppsManager;
+
+  function LoadAllNotifications(store) {
+    return new Promise( (resolve, reject) => {
+      var storedNotifications = {};
+      var cursor = store.sync();
+      function cursorResolve(task) {
+        switch (task.operation) {
+          case 'update':
+          case 'add':
+            console.log('[el] got ADD/UPD task from cursor');
+            console.log('[el] ADD/UPD | task ' + JSON.stringify(task));
+            storedNotifications[task.id] = task.data;
+            break;
+
+          case 'remove':
+            console.log('[el] got REMOVE task from cursor');
+            delete storedNotifications[task.id];
+            break;
+
+          case 'clear':
+            console.log('[el] got CLEAR task from cursor');
+            storedNotifications = {};
+            break;
+
+          case 'done':
+            console.log('[el] got DONE task| returning = ' + JSON.stringify(storedNotifications));
+            resolve(storedNotifications);
+            return;
+        }
+
+        cursor.next().then(cursorResolve, reject);
+      };
+      cursor.next().then(cursorResolve, reject);
+
+    });
+  }
+
+  function CleanAllBubbles() {
+    app.getPinAppList().forEach( app => {
+      ShowNotifBubble(app.manifestURL, 0);
+    });
+  }
+
+  function ShowNotifBubble(appId,notifCount){
+
+      var els = document.getElementsByTagName('div');
+      var i = 0;
+
+      for (i = 0; i < els.length; i++) {
+        if (els[i].hasAttribute('data-manifesturl')) {
+          if (els[i].getAttribute('data-manifesturl') == appId) {
+
+            var unreadNotif = els[i].getElementsByClassName('unread_notif')[0];
+
+            /* start test part */
+//            var notifCountBase = parseInt(unreadNotif.innerHTML || 0);
+//            notifCountBase += notifCount;
+
+//            if(notifCountBase > 999){
+//              notifCountBase = 0;
+//            }
+            /* end test part */
+
+            /* uncomment for real life */
+             var notifCountBase;
+             notifCountBase = parseInt(notifCount);
+            /* /uncomment for real life */
+
+            if(notifCountBase > 0 || notifCountBase){
+              unreadNotif.style.display = "block";
+              unreadNotif.innerHTML = notifCountBase;
+            }
+            else{
+              unreadNotif.style.display = "none";
+              unreadNotif.innerHTML = 0;
+            }
+          }
+        }
+      }
+  };
+
+  window.addEventListener("keydown", function(e) {
+    switch (e.keyCode) {
+      case 49:
+        ShowNotifBubble('app://communications.gaiamobile.org/manifest.webapp',1);
+        break;
+
+      case 50:
+        ShowNotifBubble('app://sms.gaiamobile.org/manifest.webapp',1);
+        break;
+
+       case 51:
+        ShowNotifBubble('app://camera.gaiamobile.org/manifest.webapp',1);
+        break;
+
+       case 52:
+        ShowNotifBubble('app://settings.gaiamobile.org/manifest.webapp',1);
+        break;
+
+        default:
+          return;
+        break;
+    }
+  });
+
 })(window);
